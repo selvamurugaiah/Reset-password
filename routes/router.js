972137d7784e61e67f7aa26694c1755d61f30dc5
selvamurugaiah <selvamurugaiah100@gmail.com>
2023-05-05 23:50:22 +0530

    final

--- routes/router.js
+++ routes/router.js
@@ -170,7 +170,7 @@ router.post("/sendpasswordlink",async(req,res)=>{
                 from:process.env.EMAIL,
                 to:email,
                 subject:"Sending Email For password Reset",
-                text:`This Link Valid For 2 MINUTES http://localhost:3000/forgotpassword/${userfind.id}/${setusertoken.verifytoken}`
+                text:`This Link Valid For 2 MINUTES https://super-youtiao-470e8c.netlify.app/forgotpassword/${userfind.id}/${setusertoken.verifytoken}`
             }
 
             transporter.sendMail(mailOptions,(error,info)=>{
